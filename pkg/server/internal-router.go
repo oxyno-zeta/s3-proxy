da0cb474d314cb801bddafd22c2a0d7f01aab970
Havrileck Alexandre <havrileckalexandre@gmail.com>
2019-10-06 00:03:12 +0200

    feat: Add middleware to internal server

--- pkg/server/internal-router.go
+++ pkg/server/internal-router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/dimiro1/health"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/oxyno-zeta/s3-proxy/pkg/config"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
@@ -13,6 +14,15 @@ import (
 // GenerateInternalRouter Generate internal router
 func GenerateInternalRouter(logger *logrus.Logger, cfg *config.Config) http.Handler {
 	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.DefaultCompress)
+	r.Use(middleware.NoCache)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(NewStructuredLogger(logger))
+	r.Use(middleware.Recoverer)
+
 	healthHandler := health.NewHandler()
 	// Listen path
 	r.Handle("/metrics", promhttp.Handler())
