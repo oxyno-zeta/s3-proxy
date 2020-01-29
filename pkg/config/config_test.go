6ee8ca7a118bce4beaff9eeea2dc9683edcae208
Havrileck Alexandre <havrileckalexandre@gmail.com>
2020-01-29 23:48:29 +0100

    tests: Add tests for bucket configuration object

---
+++ pkg/config/config_test.go
@@ -0,0 +1,57 @@
+package config
+
+import "testing"
+
+func TestBucketConfig_GetRootPrefix(t *testing.T) {
+	type fields struct {
+		Name        string
+		Prefix      string
+		Region      string
+		S3Endpoint  string
+		Credentials *BucketCredentialConfig
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "Must add a / at the end",
+			fields: fields{
+				Name:   "bucket",
+				Prefix: "test",
+			},
+			want: "test/",
+		},
+		{
+			name: "Must let prefix as provided",
+			fields: fields{
+				Name:   "bucket",
+				Prefix: "test/",
+			},
+			want: "test/",
+		},
+		{
+			name: "Must let empty prefix",
+			fields: fields{
+				Name:   "bucket",
+				Prefix: "",
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bcfg := &BucketConfig{
+				Name:        tt.fields.Name,
+				Prefix:      tt.fields.Prefix,
+				Region:      tt.fields.Region,
+				S3Endpoint:  tt.fields.S3Endpoint,
+				Credentials: tt.fields.Credentials,
+			}
+			if got := bcfg.GetRootPrefix(); got != tt.want {
+				t.Errorf("BucketConfig.GetRootPrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
