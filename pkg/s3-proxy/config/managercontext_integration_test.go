ac7ce4f2727d4216ffcd41c17e8cacee834482d8
Havrileck Alexandre <havrileckalexandre@gmail.com>
2020-06-10 23:00:53 +0200

    fix(config): Fix ignore hidden files and directories

--- pkg/s3-proxy/config/managercontext_integration_test.go
+++ pkg/s3-proxy/config/managercontext_integration_test.go
@@ -1300,3 +1300,142 @@ authProviders:
 		assert.FailNow(t, "shouldn't call this")
 	}
 }
+
+func Test_Load_reload_config_ignore_hidden_file_and_directory(t *testing.T) {
+	// Channel for wait watch
+	waitCh := make(chan bool)
+
+	dir, err := ioutil.TempDir("", "s3-proxy-config-reload-ignore")
+	assert.NoError(t, err)
+	err = os.MkdirAll(path.Join(dir, "dir1"), os.ModePerm)
+	assert.NoError(t, err)
+
+	configs := map[string]string{
+		"..log.yaml": `
+log:
+  level: error
+`,
+		".log2.yaml": `
+log:
+  format: fake
+`,
+		"dir1/log2.yaml": `
+server:
+  port: 8181
+`,
+		"providers.yaml": `
+authProviders:
+  basic:
+    provider1:
+      realm: prov1
+    provider2:
+      realm: prov2
+`,
+		"targets.yaml": `
+targets:
+- name: test
+  mount:
+    path: /test/
+  bucket:
+    name: bucket1
+    region: us-east-1
+    credentials:
+      accessKey:
+        path: ` + os.TempDir() + `/secret1
+      secretKey:
+        value: value2`,
+	}
+
+	defer os.RemoveAll(dir) // clean up
+	for k, v := range configs {
+		tmpfn := filepath.Join(dir, k)
+		err = ioutil.WriteFile(tmpfn, []byte(v), 0666)
+		assert.NoError(t, err)
+	}
+
+	secretFiles := map[string]string{
+		os.TempDir() + "/secret1": "VALUE1",
+	}
+	// Create secret files
+	for k, v := range secretFiles {
+		dirToCr := filepath.Dir(k)
+		err = os.MkdirAll(dirToCr, 0666)
+		assert.NoError(t, err)
+		err = ioutil.WriteFile(k, []byte(v), 0666)
+		assert.NoError(t, err)
+		defer os.Remove(k)
+	}
+
+	// Change var for main configuration file
+	mainConfigFolderPath = dir
+
+	ctx := &managercontext{
+		logger: log.NewLogger(),
+	}
+
+	ctx.AddOnChangeHook(func() {
+		waitCh <- true
+	})
+
+	// Load config
+	err = ctx.Load()
+	assert.NoError(t, err)
+	// Get configuration
+	res := ctx.GetConfig()
+
+	assert.Equal(t, &Config{
+		Log: &LogConfig{
+			Level:  "info",
+			Format: "json",
+		},
+		Server: &ServerConfig{
+			Port: 8080,
+		},
+		InternalServer: &ServerConfig{
+			Port: 9090,
+		},
+		Templates: &TemplateConfig{
+			FolderList:          "templates/folder-list.tpl",
+			TargetList:          "templates/target-list.tpl",
+			NotFound:            "templates/not-found.tpl",
+			InternalServerError: "templates/internal-server-error.tpl",
+			Unauthorized:        "templates/unauthorized.tpl",
+			Forbidden:           "templates/forbidden.tpl",
+			BadRequest:          "templates/bad-request.tpl",
+		},
+		AuthProviders: &AuthProviderConfig{
+			Basic: map[string]*BasicAuthConfig{
+				"provider1": {Realm: "prov1"},
+				"provider2": {Realm: "prov2"},
+			},
+		},
+		ListTargets: &ListTargetsConfig{
+			Enabled: false,
+		},
+		Targets: []*TargetConfig{
+			{
+				Name: "test",
+				Mount: &MountConfig{
+					Path: []string{"/test/"},
+				},
+				Bucket: &BucketConfig{
+					Name:   "bucket1",
+					Region: "us-east-1",
+					Credentials: &BucketCredentialConfig{
+						AccessKey: &CredentialConfig{
+							Value: "VALUE1",
+							Path:  path.Join(os.TempDir(), "/secret1"),
+						},
+						SecretKey: &CredentialConfig{
+							Value: "value2",
+						},
+					},
+				},
+				Actions: &ActionsConfig{
+					GET: &GetActionConfig{Enabled: true},
+				},
+				Templates: &TargetTemplateConfig{},
+			},
+		},
+	}, res)
+}
