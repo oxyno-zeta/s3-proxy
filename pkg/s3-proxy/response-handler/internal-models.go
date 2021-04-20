d044d8a678836a7c9fb2ab12934515182f067000
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-04-21 00:06:28 +0200

    feat(responsehandler): Add user into all template data

--- pkg/s3-proxy/response-handler/internal-models.go
+++ pkg/s3-proxy/response-handler/internal-models.go
@@ -3,21 +3,24 @@ package responsehandler
 import (
 	"net/http"
 
+	"github.com/oxyno-zeta/s3-proxy/pkg/s3-proxy/authx/models"
 	"github.com/oxyno-zeta/s3-proxy/pkg/s3-proxy/config"
 )
 
 // bucketListingData Bucket listing data for templating.
 type bucketListingData struct {
 	Request    *http.Request
+	User       models.GenericUser
 	Entries    []*Entry
 	BucketName string
 	Name       string
-	Path       string // Deprecated
+	Path       string
 }
 
 // errorData represents the structure used by error templating.
 type errorData struct {
 	Request *http.Request
+	User    models.GenericUser
 	Path    string
 	Error   error
 }
@@ -25,5 +28,6 @@ type errorData struct {
 // targetListData represents the structure used by target list templating.
 type targetListData struct {
 	Request *http.Request
+	User    models.GenericUser
 	Targets map[string]*config.TargetConfig
 }
