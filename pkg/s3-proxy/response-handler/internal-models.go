65793c208873970537b4014509b2b3db8984cf16
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-04-20 22:06:44 +0200

    feat(responsehandler): Add request data to all response data

--- pkg/s3-proxy/response-handler/internal-models.go
+++ pkg/s3-proxy/response-handler/internal-models.go
@@ -1,22 +1,63 @@
 package responsehandler
 
-import "github.com/oxyno-zeta/s3-proxy/pkg/s3-proxy/config"
+import (
+	"github.com/oxyno-zeta/s3-proxy/pkg/s3-proxy/config"
+)
 
 // bucketListingData Bucket listing data for templating.
 type bucketListingData struct {
+	Request    *requestData
 	Entries    []*Entry
 	BucketName string
 	Name       string
-	Path       string
+	Path       string // Deprecated
 }
 
 // errorData represents the structure used by error templating.
 type errorData struct {
-	Path  string
-	Error error
+	Request *requestData
+	Path    string
+	Error   error
 }
 
 // targetListData represents the structure used by target list templating.
 type targetListData struct {
+	Request *requestData
 	Targets map[string]*config.TargetConfig
+}
+
+// requestData represents the structure of a request.
+type requestData struct {
+	Method        string
+	Proto         string
+	ProtoMajor    int
+	ProtoMinor    int
+	Headers       map[string]interface{}
+	ContentLength int64
+	Close         bool
+	Host          string
+	RemoteAddr    string
+	RequestURI    string
+	URL           *urlData
+}
+
+// urlData represents the structure of an url structure in a request.
+type urlData struct { //nolint:maligned // Ignored for visibility
+	Scheme      string
+	Opaque      string
+	Host        string
+	Path        string
+	RawPath     string
+	ForceQuery  bool
+	RawQuery    string
+	Fragment    string
+	RawFragment string
+	// After this line, values are coming from functions
+	IsAbs           bool
+	EscapedFragment string
+	EscapedPath     string
+	Port            string
+	RequestURI      string
+	String          string
+	QueryParams     map[string]interface{}
 }
