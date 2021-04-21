3e5b0810ab22549d0b729a05912545eb91c34232
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-04-21 22:56:24 +0200

    feat(authx): Add more getter to generic user

--- pkg/s3-proxy/authx/models/generic-user.go
+++ pkg/s3-proxy/authx/models/generic-user.go
@@ -2,8 +2,22 @@ package models
 
 // Generic user interface used to know specific type of user.
 type GenericUser interface {
-	// Get type of user
+	// Get type of user (OIDC or BASIC).
 	GetType() string
-	// Get identifier
+	// Get identifier (Username for basic auth user or Username or email for OIDC user).
 	GetIdentifier() string
+	// Get username.
+	GetUsername() string
+	// Get name (only available for OIDC user).
+	GetName() string
+	// Get groups (only available for OIDC user).
+	GetGroups() []string
+	// Get given name (only available for OIDC user).
+	GetGivenName() string
+	// Get family name (only available for OIDC user).
+	GetFamilyName() string
+	// Get email (only available for OIDC user).
+	GetEmail() string
+	// Is Email Verified ? (only available for OIDC user).
+	IsEmailVerified() bool
 }
