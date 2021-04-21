f6c8d36f0ce509f8dd65c66b0ff4a16eeb51ae12
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-04-21 22:56:24 +0200

    feat(authx): Add more getter to generic user

--- pkg/s3-proxy/authx/models/basic-auth-user.go
+++ pkg/s3-proxy/authx/models/basic-auth-user.go
@@ -13,3 +13,38 @@ func (u *BasicAuthUser) GetType() string {
 func (u *BasicAuthUser) GetIdentifier() string {
 	return u.Username
 }
+
+// Get username.
+func (u *BasicAuthUser) GetUsername() string {
+	return u.Username
+}
+
+// Get name (only available for OIDC user).
+func (u *BasicAuthUser) GetName() string {
+	return ""
+}
+
+// Get groups (only available for OIDC user).
+func (u *BasicAuthUser) GetGroups() []string {
+	return nil
+}
+
+// Get given name (only available for OIDC user).
+func (u *BasicAuthUser) GetGivenName() string {
+	return ""
+}
+
+// Get family name (only available for OIDC user).
+func (u *BasicAuthUser) GetFamilyName() string {
+	return ""
+}
+
+// Get email (only available for OIDC user).
+func (u *BasicAuthUser) GetEmail() string {
+	return ""
+}
+
+// Is Email Verified ? (only available for OIDC user).
+func (u *BasicAuthUser) IsEmailVerified() bool {
+	return false
+}
