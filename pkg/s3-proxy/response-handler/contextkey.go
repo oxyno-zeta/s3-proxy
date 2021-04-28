78684eac2bc792579bb80deb7ae851bf850b98b3
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-04-28 09:08:56 +0200

    refactor(responsehandler): Refactor code and add set response handler in context

--- pkg/s3-proxy/response-handler/contextkey.go
+++ pkg/s3-proxy/response-handler/contextkey.go
@@ -1,8 +1,22 @@
 package responsehandler
 
+import "context"
+
 // contextKey is a value for use with context.WithValue. It's used as
 // a pointer so it fits in an interface{} without allocation. This technique
 // for defining context keys was copied from Go 1.7's new use of context in net/http.
 type contextKey struct {
 	name string
+}
+
+var responseHandlerCtxKey = &contextKey{name: "ResponseHandlerCtxKey"}
+
+// GetResponseHandlerFromContext will return the response handler object from context.
+func GetResponseHandlerFromContext(ctx context.Context) ResponseHandler {
+	return ctx.Value(responseHandlerCtxKey).(ResponseHandler)
+}
+
+// SetResponseHandlerInContext will set a response handler object in a context.
+func SetResponseHandlerInContext(ctx context.Context, resH ResponseHandler) context.Context {
+	return context.WithValue(ctx, responseHandlerCtxKey, resH)
 }
