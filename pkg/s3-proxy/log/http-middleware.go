97447741f35e7a7e8ccbc5848c6e34bc84523f37
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-05-01 01:08:53 +0200

    feat(log): Simplify logger construction

--- pkg/s3-proxy/log/http-middleware.go
+++ pkg/s3-proxy/log/http-middleware.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/oxyno-zeta/s3-proxy/pkg/s3-proxy/utils"
 	"github.com/sirupsen/logrus"
 )
 
@@ -33,23 +34,17 @@ func HTTPAddLoggerToContextMiddleware() func(next http.Handler) http.Handler {
 func NewStructuredLogger(
 	logger Logger,
 	getTraceID func(r *http.Request) string,
-	getClientIP func(r *http.Request) string,
-	getRequestURI func(r *http.Request) string,
 ) func(next http.Handler) http.Handler {
 	return middleware.RequestLogger(&StructuredLogger{
-		Logger:        logger,
-		GetTraceID:    getTraceID,
-		GetClientIP:   getClientIP,
-		GetRequestURI: getRequestURI,
+		Logger:     logger,
+		GetTraceID: getTraceID,
 	})
 }
 
 // StructuredLogger structured logger.
 type StructuredLogger struct {
-	Logger        Logger
-	GetTraceID    func(r *http.Request) string
-	GetClientIP   func(r *http.Request) string
-	GetRequestURI func(r *http.Request) string
+	Logger     Logger
+	GetTraceID func(r *http.Request) string
 }
 
 // NewLogEntry new log entry.
@@ -67,20 +62,15 @@ func (l *StructuredLogger) NewLogEntry(r *http.Request) middleware.LogEntry {
 		logFields["req_id"] = reqID
 	}
 
-	scheme := "http"
-	if r.TLS != nil {
-		scheme = "https"
-	}
-
-	logFields["http_scheme"] = scheme
+	logFields["http_scheme"] = utils.GetRequestScheme(r)
 	logFields["http_proto"] = r.Proto
 	logFields["http_method"] = r.Method
 
 	logFields["remote_addr"] = r.RemoteAddr
 	logFields["user_agent"] = r.UserAgent()
-	logFields["client_ip"] = l.GetClientIP(r)
+	logFields["client_ip"] = utils.ClientIP(r)
 
-	logFields["uri"] = l.GetRequestURI(r)
+	logFields["uri"] = utils.GetRequestURI(r)
 
 	entry.Logger = entry.Logger.WithFields(logFields)
 
