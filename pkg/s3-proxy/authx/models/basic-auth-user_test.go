35bf34e5d9d04abde424f01f6a51868c311d81ea
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-05-01 22:52:30 +0200

    test(models): Add unit tests on user models

--- pkg/s3-proxy/authx/models/basic-auth-user_test.go
+++ pkg/s3-proxy/authx/models/basic-auth-user_test.go
@@ -3,6 +3,7 @@
 package models
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -64,6 +65,209 @@ func TestBasicAuthUser_GetType(t *testing.T) {
 			}
 			if got := u.GetType(); got != tt.want {
 				t.Errorf("BasicAuthUser.GetType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBasicAuthUser_GetUsername(t *testing.T) {
+	type fields struct {
+		Username string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Username: "basic",
+			},
+			want: "basic",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &BasicAuthUser{
+				Username: tt.fields.Username,
+			}
+			if got := u.GetUsername(); got != tt.want {
+				t.Errorf("BasicAuthUser.GetUsername() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBasicAuthUser_GetName(t *testing.T) {
+	type fields struct {
+		Username string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Username: "basic",
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &BasicAuthUser{
+				Username: tt.fields.Username,
+			}
+			if got := u.GetName(); got != tt.want {
+				t.Errorf("BasicAuthUser.GetName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBasicAuthUser_GetGroups(t *testing.T) {
+	type fields struct {
+		Username string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Username: "basic",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &BasicAuthUser{
+				Username: tt.fields.Username,
+			}
+			if got := u.GetGroups(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("BasicAuthUser.GetGroups() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBasicAuthUser_GetGivenName(t *testing.T) {
+	type fields struct {
+		Username string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Username: "basic",
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &BasicAuthUser{
+				Username: tt.fields.Username,
+			}
+			if got := u.GetGivenName(); got != tt.want {
+				t.Errorf("BasicAuthUser.GetGivenName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBasicAuthUser_GetFamilyName(t *testing.T) {
+	type fields struct {
+		Username string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Username: "basic",
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &BasicAuthUser{
+				Username: tt.fields.Username,
+			}
+			if got := u.GetFamilyName(); got != tt.want {
+				t.Errorf("BasicAuthUser.GetFamilyName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBasicAuthUser_GetEmail(t *testing.T) {
+	type fields struct {
+		Username string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Username: "basic",
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &BasicAuthUser{
+				Username: tt.fields.Username,
+			}
+			if got := u.GetEmail(); got != tt.want {
+				t.Errorf("BasicAuthUser.GetEmail() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestBasicAuthUser_IsEmailVerified(t *testing.T) {
+	type fields struct {
+		Username string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "valid",
+			fields: fields{
+				Username: "basic",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			u := &BasicAuthUser{
+				Username: tt.fields.Username,
+			}
+			if got := u.IsEmailVerified(); got != tt.want {
+				t.Errorf("BasicAuthUser.IsEmailVerified() = %v, want %v", got, tt.want)
 			}
 		})
 	}
