15dc32f23e8e4ec0c9b5887e47e2269f891cfcc5
Havrileck Alexandre <havrileckalexandre@gmail.com>
2021-07-01 23:25:33 +0200

    feat(logger): Display stack trace when possible and when an error is provided to logger.Error

--- pkg/s3-proxy/log/instance.go
+++ pkg/s3-proxy/log/instance.go
@@ -1,14 +1,22 @@
 package log
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/pkg/errors"
 	logrus "github.com/sirupsen/logrus"
 )
 
 type loggerIns struct {
 	logrus.FieldLogger
+}
+
+// This is dirty pkg/errors.
+type stackTracer interface {
+	StackTrace() errors.StackTrace
 }
 
 func (ll *loggerIns) GetTracingLogger() TracingLogger {
@@ -87,7 +95,53 @@ func (ll *loggerIns) WithError(err error) Logger {
 	// Create new field logger
 	fieldL := ll.FieldLogger.WithError(err)
 
-	return &loggerIns{
-		FieldLogger: fieldL.Logger,
+	addStackTrace := func(pError stackTracer) {
+		// Get stack trace from error
+		st := pError.StackTrace()
+		// Stringify stack trace
+		valued := fmt.Sprintf("%+v", st)
+		// Remove all tabs
+		valued = strings.ReplaceAll(valued, "\t", "")
+		// Split on new line
+		stack := strings.Split(valued, "\n")
+		// Remove first empty string
+		stack = stack[1:]
+		// Add stack trace to field logger
+		fieldL = fieldL.WithField("stack", strings.Join(stack, ","))
 	}
+
+	// Check if error is matching stack trace interface
+	// nolint: errorlint // Ignore this because the aim is to catch stack trace error at first level
+	if err2, ok := err.(stackTracer); ok {
+		addStackTrace(err2)
+	}
+
+	// Check if error cause is matching stack trace interface
+	// nolint: errorlint // Ignore this because the aim is to catch stack trace error at first level
+	if err2, ok := errors.Cause(err).(stackTracer); ok {
+		addStackTrace(err2)
+	}
+
+	return &loggerIns{
+		FieldLogger: fieldL,
+	}
+}
+
+func (ll *loggerIns) Error(args ...interface{}) {
+	// Get first element
+	elem := args[0]
+
+	// Try to cast element to error
+	err, ok := elem.(error)
+	// Check if can be casted to error
+	if ok {
+		// Call with error
+		res := ll.WithError(err)
+
+		// Change internal field logger
+		ll.FieldLogger = res.(*loggerIns).FieldLogger
+	}
+
+	// Call logger error method
+	ll.FieldLogger.Error(args...)
 }
