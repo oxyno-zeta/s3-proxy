68cfeafd1032f7b34c8228186e3017d614e9e5d7
Havrileck Alexandre <havrileckalexandre@gmail.com>
2024-02-13 23:09:02 +0100

    refactor(metrics): Rename variable

--- pkg/s3-proxy/metrics/prometheus.go
+++ pkg/s3-proxy/metrics/prometheus.go
@@ -23,7 +23,7 @@ type prometheusClient struct {
 }
 
 // Instrument will instrument gin routes.
-func (ctx *prometheusClient) Instrument(serverLabel string) func(next http.Handler) http.Handler {
+func (cl *prometheusClient) Instrument(serverLabel string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Begin timer
@@ -43,10 +43,10 @@ func (ctx *prometheusClient) Instrument(serverLabel string) func(next http.Handl
 			resSz := float64(sw.length)
 
 			// Manage prometheus metrics
-			ctx.reqDur.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Observe(elapsed)
-			ctx.reqCnt.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Inc()
-			ctx.reqSz.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Observe(float64(reqSz))
-			ctx.resSz.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Observe(resSz)
+			cl.reqDur.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Observe(elapsed)
+			cl.reqCnt.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Inc()
+			cl.reqSz.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Observe(float64(reqSz))
+			cl.resSz.WithLabelValues(serverLabel, status, r.Method, r.Host, r.URL.Path).Observe(resSz)
 		})
 	}
 }
@@ -57,117 +57,117 @@ func (*prometheusClient) GetExposeHandler() http.Handler {
 }
 
 // IncS3Operations Increment s3 operation counter.
-func (ctx *prometheusClient) IncS3Operations(targetName, bucketName, operation string) {
-	ctx.s3OperationsTotal.WithLabelValues(targetName, bucketName, operation).Inc()
+func (cl *prometheusClient) IncS3Operations(targetName, bucketName, operation string) {
+	cl.s3OperationsTotal.WithLabelValues(targetName, bucketName, operation).Inc()
 }
 
 // Will increase counter of authenticated user.
-func (ctx *prometheusClient) IncAuthenticated(providerType, providerName string) {
-	ctx.authenticatedTotal.WithLabelValues(providerType, providerName).Inc()
+func (cl *prometheusClient) IncAuthenticated(providerType, providerName string) {
+	cl.authenticatedTotal.WithLabelValues(providerType, providerName).Inc()
 }
 
 // Will increase counter of authorized user.
-func (ctx *prometheusClient) IncAuthorized(providerType string) {
-	ctx.authorizedTotal.WithLabelValues(providerType).Inc()
+func (cl *prometheusClient) IncAuthorized(providerType string) {
+	cl.authorizedTotal.WithLabelValues(providerType).Inc()
 }
 
-func (ctx *prometheusClient) IncSucceedWebhooks(targetName, actionName string) {
-	ctx.succeedWebhooks.WithLabelValues(targetName, actionName).Inc()
+func (cl *prometheusClient) IncSucceedWebhooks(targetName, actionName string) {
+	cl.succeedWebhooks.WithLabelValues(targetName, actionName).Inc()
 }
 
-func (ctx *prometheusClient) IncFailedWebhooks(targetName, actionName string) {
-	ctx.failedWebhooks.WithLabelValues(targetName, actionName).Inc()
+func (cl *prometheusClient) IncFailedWebhooks(targetName, actionName string) {
+	cl.failedWebhooks.WithLabelValues(targetName, actionName).Inc()
 }
 
-func (ctx *prometheusClient) register() {
-	ctx.reqCnt = prometheus.NewCounterVec(
+func (cl *prometheusClient) register() {
+	cl.reqCnt = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "http_requests_total",
 			Help: "How many HTTP requests have been processed ?",
 		},
 		[]string{"server", "status_code", "method", "host", "path"},
 	)
-	prometheus.MustRegister(ctx.reqCnt)
+	prometheus.MustRegister(cl.reqCnt)
 
-	ctx.reqDur = prometheus.NewSummaryVec(
+	cl.reqDur = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
 			Name: "http_request_duration_seconds",
 			Help: "The HTTP request latencies in seconds.",
 		},
 		[]string{"server", "status_code", "method", "host", "path"},
 	)
-	prometheus.MustRegister(ctx.reqDur)
+	prometheus.MustRegister(cl.reqDur)
 
-	ctx.reqSz = prometheus.NewSummaryVec(
+	cl.reqSz = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
 			Name: "http_request_size_bytes",
 			Help: "The HTTP request sizes in bytes.",
 		},
 		[]string{"server", "status_code", "method", "host", "path"},
 	)
-	prometheus.MustRegister(ctx.reqSz)
+	prometheus.MustRegister(cl.reqSz)
 
-	ctx.resSz = prometheus.NewSummaryVec(
+	cl.resSz = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
 			Name: "http_response_size_bytes",
 			Help: "The HTTP response sizes in bytes.",
 		},
 		[]string{"server", "status_code", "method", "host", "path"},
 	)
-	prometheus.MustRegister(ctx.resSz)
+	prometheus.MustRegister(cl.resSz)
 
-	ctx.up = prometheus.NewGaugeVec(
+	cl.up = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "up",
 			Help: "1 = up, 0 = down",
 		},
 		[]string{"component"},
 	)
-	ctx.up.WithLabelValues("s3-proxy").Set(1)
-	prometheus.MustRegister(ctx.up)
+	cl.up.WithLabelValues("s3-proxy").Set(1)
+	prometheus.MustRegister(cl.up)
 
-	ctx.s3OperationsTotal = prometheus.NewCounterVec(
+	cl.s3OperationsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "s3_operations_total",
 			Help: "How many operations are generated to s3 in total ?",
 		},
 		[]string{"target_name", "bucket_name", "operation"},
 	)
-	prometheus.MustRegister(ctx.s3OperationsTotal)
+	prometheus.MustRegister(cl.s3OperationsTotal)
 
-	ctx.authenticatedTotal = prometheus.NewCounterVec(
+	cl.authenticatedTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "authenticated_total",
 			Help: "How many users have been authenticated ?",
 		},
 		[]string{"provider_type", "provider_name"},
 	)
-	prometheus.MustRegister(ctx.authenticatedTotal)
+	prometheus.MustRegister(cl.authenticatedTotal)
 
-	ctx.authorizedTotal = prometheus.NewCounterVec(
+	cl.authorizedTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "authorized_total",
 			Help: "How many users have been authorized ?",
 		},
 		[]string{"provider_type"},
 	)
-	prometheus.MustRegister(ctx.authorizedTotal)
+	prometheus.MustRegister(cl.authorizedTotal)
 
-	ctx.succeedWebhooks = prometheus.NewCounterVec(
+	cl.succeedWebhooks = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "succeed_webhooks_total",
 			Help: "How many webhooks have been succeed ?",
 		},
 		[]string{"target_name", "action_name"},
 	)
-	prometheus.MustRegister(ctx.succeedWebhooks)
+	prometheus.MustRegister(cl.succeedWebhooks)
 
-	ctx.failedWebhooks = prometheus.NewCounterVec(
+	cl.failedWebhooks = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "failed_webhooks_total",
 			Help: "How many webhooks have been failed ?",
 		},
 		[]string{"target_name", "action_name"},
 	)
-	prometheus.MustRegister(ctx.failedWebhooks)
+	prometheus.MustRegister(cl.failedWebhooks)
 }
